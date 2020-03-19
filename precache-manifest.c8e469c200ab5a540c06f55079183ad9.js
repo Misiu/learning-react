8a7886373b4bc95018444ab494478ee2cffb52df
Misiu <t.jagusz@gmail.com>
2020-03-19 14:08:29 +0000

    Deploying to gh-pages from master eb51b43d154bf670295a67c4a915b20023d88794

---
+++ precache-manifest.c8e469c200ab5a540c06f55079183ad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e36dd993db924cbd4344b7173fd3d75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49550c401d9d5407f587",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9ff1c44fc55b5e459b2d",
+    "url": "./static/js/2.3bc369ee.chunk.js"
+  },
+  {
+    "revision": "49550c401d9d5407f587",
+    "url": "./static/js/main.daa0825b.chunk.js"
+  },
+  {
+    "revision": "266435f1527a8af11922",
+    "url": "./static/js/runtime~main.42a90f6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
